4e3e234fa2f0f5426cefd683eb0470c395a7b3c9
zhongh08 <zhongh08@163.com>
2019-07-05 19:41:39 +0800

    Initial commit

---
+++ src/main/java/com/syoki/springcloudfeign/SchedualServiceHi.java
@@ -0,0 +1,23 @@
+package com.syoki.springcloudfeign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "service-hi",fallback = SchedualServiceHiHystric.class)
+public interface SchedualServiceHi {
+
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+
+}
+
+@Component
+class SchedualServiceHiHystric implements SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry "+name;
+    }
+}
